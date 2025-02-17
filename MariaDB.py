4d06771f6f61db36a7a3edbff841f9aa9a052437
Noelia <128001264+noeliamed@users.noreply.github.com>
2025-02-17 13:10:44 +0100

    Update MariaDB.py

--- MariaDB.py
+++ MariaDB.py
@@ -8,3 +8,29 @@ opcion_elegida=0
 conn = mysql.connector.connect(user="noelia", password="noelia", host="192.168.105.132", database="consultas")
 cursor = conn.cursor()
 
+while opcion_elegida != 6 :
+    opcion_elegida=MariaDBmenu()
+    
+    # 0. Se introduce el nombre de un artista y muestra una lista con cuántos tours ha realizado.
+    if opcion_elegida == 0:
+        MariaDB_ArtistasTours(cursor)
+    # 1. Se introduce el nombre de un artista y se filtra todos los álbumes que ha lanzado.
+    elif opcion_elegida == 1:
+        MariaDB_AlbumesPorArtista(cursor)
+    # 2. Se introduce una letra y lista los álbumes de los artistas cuyo nombre empiece con esa letra.
+    elif opcion_elegida == 2:
+        MariaDB_ListarAlbumesArtistasPorLetra(cursor)
+    # 3. Se introducen los datos de un nuevo tour (nombre del tour, fechas de inicio y fin, países visitados) y se insertan en la tabla de tours.
+    elif opcion_elegida == 3:
+        MariaDBInsertarTour(cursor)
+    # 4. Se eliminan los artistas cuyo nombre tenga más de 15 caracteres.
+    elif opcion_elegida == 4:
+        MariaDBBorrarArtistas15(cursor)
+    # 5. Introduce el nntroduce el nombre de un tour y actualiza el país que ha visitado el artista.
+    elif opcion_elegida == 5:
+        MariaDBActualizarPaises(cursor)
+
+
+if opcion_elegida==6:
+    cursor.close()
+    conn.close()
