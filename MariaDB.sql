fb5ab75b78f7b0b8810fae8e8cbdc3aeddfac382
Noelia <128001264+noeliamed@users.noreply.github.com>
2025-02-18 10:16:05 +0100

    Update MariaDB.sql

--- MariaDB.sql
+++ MariaDB.sql
@@ -1,5 +1,7 @@
+--Proyecto BD con Python. MariaDB
+
 CREATE TABLE artistas (
-artista_id VARCHAR(4)
+artista_id VARCHAR(4),
 nombre VARCHAR(20) NOT NULL,                
 estilo VARCHAR(10),               
 fecha_debut DATE,
@@ -9,25 +11,34 @@ CONSTRAINT pk_artistas_id PRIMARY KEY (artista_id)
 
 CREATE TABLE albumes (
 album_id VARCHAR(5) NOT NULL,
-artista_id VARCHAR(4) NOT NULL,
+nombre_artista VARCHAR(20) NOT NULL,
 nombre_album VARCHAR(20) NOT NULL,
 fecha_lanzamiento DATE,     
-COSNTRAINT pk_albumes_albID PRIMARY KEY (album_id),
-CONSTRAINT fk_albumes_artista FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
+CONSTRAINT pk_album_ID PRIMARY KEY (album_id)
 );
+
+--CONSTRAINT fk_albumes_artista FOREIGN KEY (nombre_artista) REFERENCES artistas(nombre)
+
+
 
 CREATE TABLE tours (
 tour_id VARCHAR(5) NOT NULL,     
-artista_id VARCHAR(4) NOT NULL,                                
+nombre_artista VARCHAR(20) NOT NULL,                                
 nombre_tour VARCHAR(20) NOT NULL,           
 fecha_inicio DATE,                            
 fecha_fin DATE,                               
-paises_visitados VARCHAR2(15),    
-CONSTRAINT pk_tours_id PRIMARY KEY (tour_id),
-CONSTRAINT fk_tours_artID FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
+paises_visitados VARCHAR(30),    
+CONSTRAINT pk_tours_id PRIMARY KEY (tour_id)
 );
 
---Artistas: 
+INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('34569', 'BTS World Tour', '2019-05-01', '2020-02-21', 'Estados Unidos, Japón, Corea del Sur, Europa');
+
+
+--CONSTRAINT fk_tours_artID FOREIGN KEY (nombre_artista) REFERENCES artistas(nombre)
+
+
+
+--Artistas:
 INSERT INTO artistas (artista_id, nombre, estilo, fecha_debut) VALUES ('7771', 'BTS', 'kpop', '2013-06-13');
 INSERT INTO artistas (artista_id, nombre, estilo, fecha_debut) VALUES ('1372', 'Taylor Swift', 'pop', '2006-10-24');
 INSERT INTO artistas (artista_id, nombre, estilo, fecha_debut) VALUES ('5623', 'BLACKPINK', 'kpop', '2016-08-08');
@@ -39,7 +50,8 @@ INSERT INTO artistas (artista_id, nombre, estilo, fecha_debut) VALUES ('3458', '
 INSERT INTO artistas (artista_id, nombre, estilo, fecha_debut) VALUES ('4579', 'ATEEZ', 'kpop', '2018-10-24');
 INSERT INTO artistas (artista_id, nombre, estilo, fecha_debut) VALUES ('3468', 'Sabrina Carpenter', 'pop', '2014-03-14');
 
---Álbumes
+
+--Álbumes: 
 INSERT INTO albumes (album_id, nombre_artista, nombre_album, fecha_lanzamiento) VALUES ('94320', 'BTS', 'Map of the Soul: 7', '2020-02-21');
 INSERT INTO albumes (album_id, nombre_artista, nombre_album, fecha_lanzamiento) VALUES ('43293', 'BTS', 'BE', '2020-11-20');
 INSERT INTO albumes (album_id, nombre_artista, nombre_album, fecha_lanzamiento) VALUES ('74344', 'Taylor Swift', '1989', '2014-10-27');
@@ -61,22 +73,53 @@ INSERT INTO albumes (album_id, nombre_artista, nombre_album, fecha_lanzamiento) 
 INSERT INTO albumes (album_id, nombre_artista, nombre_album, fecha_lanzamiento) VALUES ('94594', 'Sabrina Carpenter', 'Emails I Can’t Send', '2022-07-15');
 
 --Tours
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('34569', 'BTS World Tour', '2019-05-01', '2020-02-21', 'Estados Unidos, Japón, Corea del Sur, Europa');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('33470', 'BTS World Tour', 'Love Yourself World Tour', '2018-08-25', '2019-04-07', 'Estados Unidos, Canadá, Japón, Europa, Australia, Corea del Sur');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('43475', 'Taylor Swift', 'The Eras Tour', '2023-03-17', '2023-08-09', 'Estados Unidos, Canadá, México, Europa, Asia, Australia');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('34572', 'Taylor Swift', 'Red Tour', '2013-03-13', '2014-06-12', 'Estados Unidos, Canadá, Europa, Asia, Australia');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('79573', 'Taylor Swift', '1989 World Tour', '2015-05-05', '2015-12-12', 'Estados Unidos, Canadá, Asia, Australia');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('20574', 'Taylor Swift', 'Reputation Stadium Tour', '2018-05-08', '2018-11-21', 'Estados Unidos, Canadá, Reino Unido, Irlanda, Australia');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('15678', 'BLACKPINK', 'Born Pink World Tour', '2022-10-15', '2023-09-17', 'Corea del Sur, Estados Unidos, Canadá, Europa, Asia, Australia, México');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('74564', 'BLACKPINK', 'BLACKPINK Arena Tour', '2018-07-24', '2018-12-24', 'Japón');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('34539', 'Abraham Mateo', 'Who I Am Tour', '2014-10-03', '2015-07-26', 'España, México, Argentina, Chile');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('75682', 'Shawn Mendes', 'Shawn Mendes: The Tour', '2019-03-07', '2019-12-21', 'Estados Unidos, Canadá, Europa, Asia, Australia, América Latina');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('03483', 'Olivia Rodrigo', 'SOUR Tour', '2022-04-05', '2022-07-07', 'Estados Unidos, Canadá, Europa');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('86745', 'The Weeknd', 'After Hours Til Dawn Tour', '2022-07-14', '2023-10-25', 'Estados Unidos, América Latina, Asia, Australia');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('37566', 'Coldplay', 'A Head Full of Dreams Tour', '2016-03-31', '2017-11-15', 'Estados Unidos, Canadá, Europa, Asia, América Latina, Australia');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('94569', 'ATEEZ', 'The Fellowship: Break the Wall Tour', '2022-10-29', '2023-05-08', 'Estados Unidos, Europa, Asia, América Latina');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('65470', 'Sabrina Carpenter', 'Emails I Can’t Send Tour', '2022-09-28', '2023-06-25', 'Estados Unidos, Europa, Asia');
-INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) VALUES ('35530', 'Sabrina Carpenter', 'Evolution Tour', '2016-10-18', '2017-05-21', 'Estados Unidos, Canadá');
+
+INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) 
+VALUES 
+('TS001', 'Taylor Swift', 'Eras Tour', '2023-03-01', '2023-12-01', 'EE.UU, Canadá, Reino Unido'),
+('TS002', 'Taylor Swift', 'Fearless Tour', '2021-06-01', '2021-11-30', 'Australia, Japón');
+
+
+INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) 
+VALUES 
+('BTS01', 'BTS', 'Love Yourself Tour', '2018-08-01', '2019-02-01', 'Corea del Sur, EE.UU, México'),
+('BTS02', 'BTS', 'Map of the Soul Tour', '2020-04-01', '2020-10-01', 'Japón, Alemania, España');
+
+
+INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) 
+VALUES 
+('BP001', 'Blackpink', 'In Your Area Tour', '2018-11-01', '2019-02-28', 'EE.UU, Australia, Singapur'),
+('BP002', 'Blackpink', 'Born Pink Tour', '2022-09-01', '2023-01-15', 'Francia, Canadá, Japón');
+
+
+INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) 
+VALUES 
+('AATZ1', 'Ateez', 'The Fellowship Tour', '2021-10-01', '2021-11-30', 'México, EE.UU, Alemania'),
+('AATZ2', 'Ateez', 'Infinity Tour', '2022-03-01', '2022-06-01', 'España, Italia, Brasil');
+
+
+INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) 
+VALUES 
+('AM001', 'Abraham Mateo', 'Lo Nuestro Tour', '2022-07-01', '2022-12-01', 'España, Colombia, Argentina'),
+('AM002', 'Abraham Mateo', 'Amor de Los Dos Tour', '2023-02-01', '2023-05-01', 'México, Chile, Ecuador');
+
+
+INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) 
+VALUES 
+('OR001', 'Olivia Rodrigo', 'Sour Tour', '2022-04-01', '2022-09-01', 'Reino Unido, Canadá, EE.UU'),
+('OR002', 'Olivia Rodrigo', 'Guts Tour', '2023-03-01', '2023-06-01', 'México, Japón, Australia');
+
+
+INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) 
+VALUES 
+('SM001', 'Shawn Mendes', 'Wonder Tour', '2021-06-01', '2021-10-31', 'Canadá, EE.UU, Brasil'),
+('SM002', 'Shawn Mendes', 'Illuminate Tour', '2017-03-01', '2017-10-01', 'Reino Unido, Francia');
+
+
+INSERT INTO tours (tour_id, nombre_artista, nombre_tour, fecha_inicio, fecha_fin, paises_visitados) 
+VALUES 
+('SC001', 'Sabrina Carpenter', 'Emails I Can’t Send', '2022-06-01', '2022-12-15', 'EE.UU, Canadá, Reino Unido'),
+('SC002', 'Sabrina Carpenter', 'Singularity Tour', '2023-01-01', '2023-06-30', 'Australia, Francia, México');
 
 
 
